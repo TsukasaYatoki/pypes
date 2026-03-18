c6fbfee30b2416da621746b32e5cf857a0177470
TsukasaYatoki <oumaekumiko@hnu.edu.cn>
2026-03-18 19:37:50 +0800

    feat: add argument parsing

--- pypes/main.py
+++ pypes/main.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 from blessed import Terminal
@@ -5,7 +6,20 @@ from pipe import Pipe
 from render import Canvas
 
 
-def main():
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="A terminal animation of pipes flowing across the screen."
+    )
+
+    parser.add_argument("--fps", type=int, default=20, help="Frames per second")
+    parser.add_argument(
+        "--straight_prob", type=float, default=0.9, help="Probability of going straight"
+    )
+
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace) -> None:
     term = Terminal()
     canvas = Canvas(term)
     canvas.clear()
@@ -16,9 +30,11 @@ def main():
         while True:
             canvas.draw(pipe)
             pipe.move()
-            pipe.turn()
-            time.sleep(0.1)
+            pipe.turn(args.straight_prob)
+            time.sleep(1 / args.fps)
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+
+    main(args)
